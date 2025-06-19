ecab5ba3866b98be96921da5f971a7e06301c3d1
Patryk Czogała <patrykczogala1810@gmail.com>
2025-06-19 15:47:06 +0200

    Dodanie funkcji tekstowego pokazywania wyniku i wizualizacje pinezek

--- src/text_interface.py
+++ src/text_interface.py
@@ -6,3 +6,14 @@ def show_main_menu():
 	print("2 - Komputer zgaduje")
 	print("3 - Tryb dwóch graczy")
 	print("4 - Wczytaj grę")
+
+
+def show_feedback(black, white, tries_left):
+    	show_pegs(black, white)
+    	print("Czarne (trafione miejsce i liczba):", black)
+    	print("Białe (trafiona liczba, złe miejsce):", white)
+    	print("Pozostało prób:", tries_left)
+    	print("------------------------")
+
+def show_pegs(black, white):
+    	print("Wskazówki:", "○" * black + "●" * white)
