61534faff66c160a045bc1343142890524196c7d
WladcaPomaranczy <krzysiek.db.04@gmail.com>
2025-06-20 00:22:00 +0200

    Ogolne działanie programu testowy commit

---
+++ Main.py
@@ -0,0 +1,38 @@
+import random
+
+Colors = ['Red', 'Green', 'White', 'Yellow', 'Orange', 'Pink']
+
+
+# def JakaDlugosc(n):
+#    ...
+
+#placeholder wyboru dlugosci kodu
+Length = 4
+
+# def IleProbZgadywania(n):
+#    ...
+
+#placeholder wyboru mozliwych prob
+MaxAttempts = 4
+
+def GenerateCode():
+    return [random.choice(Colors) for _ in range(Length)]
+
+def CheckGuess(secret, guess):
+    black_pegs = 0
+    white_pegs = 0
+    SecretCopy= secret[:]
+    GuessCopy = guess[:]
+    for i in range(Length):
+        if guess[i] == secret[i]:
+            black_pegs += 1
+            SecretCopy[i] = None
+            GuessCopy[i] = None
+    for i in range(Length):
+        if GuessCopy[i] and GuessCopy[i] in SecretCopy:
+            white_pegs += 1
+            SecretCopy[SecretCopy.index(GuessCopy[i])] = None
+
+    return black_pegs, white_pegs    
+
+
