fb703f262b3f3b623fff7924e67cd70bb506a388
Patryk Czogała <patrykczogala1810@gmail.com>
2025-06-21 18:43:35 +0200

    Dodanie importów i wstępnej funkcji

---
+++ src/user_guess.py
@@ -0,0 +1,7 @@
+from Main import GenerateCode, CheckGuess, Length
+from text_interface import show_feedback, show_win, show_loss
+
+def player_guesses_code():
+    print("Rozpoczynasz grę! Komputer ustawił tajny kod z kolorów.")
+    print("Dostępne kolory: Red, Green, White, Yellow, Orange, Pink, Purple, Blue")
+    print("Podawaj kolory oddzielone spacją, np.: Red Green Blue Yellow")
