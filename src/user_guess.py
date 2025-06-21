f4e9daf16ac498563a868fdc23794e9baddaf461
Patryk Czogała <patrykczogala1810@gmail.com>
2025-06-21 20:15:56 +0200

    Dodanie ustawień trudności z game_logic.py i opóźnienie wyjścia do menu po porażce i wygranej

--- src/user_guess.py
+++ src/user_guess.py
@@ -1,13 +1,22 @@
-from game_logic import GenerateCode, CheckGuess, Length
+import time
+from game_logic import GenerateCode, CheckGuess, GetLength, MaxNumberOfAttempts, NumberOfColors, Colors, Length
 from text_interface import show_feedback, show_win, show_loss
 
 def player_guesses_code():
     print("Rozpoczynasz grę! Komputer ustawił tajny kod z kolorów.")
-    print("Dostępne kolory: Red, Green, White, Yellow, Orange, Pink, Purple, Blue")
+
+    length = GetLength(None)
+    tries = MaxNumberOfAttempts(None)
+    color_count = NumberOfColors(None)
+    available_colors = Colors[:color_count]
+
+    print(f"\nUżywanych będzie {length} kolorów z listy:")
+    print(', '.join(available_colors))
     print("Podawaj kolory oddzielone spacją, np.: Red Green Blue Yellow")
 
+    globals()['Length'] = length
+
     secret_code = GenerateCode()
-    tries = 10
 
     for i in range(tries):
         guess_input = input(f"Próba {i+1}/{tries}: ")
@@ -17,12 +26,18 @@ def player_guesses_code():
             print(f"Musisz podać dokładnie {Length} kolorów.")
             continue
 
+        if not all(color in available_colors for color in guess):
+            print("Niepoprawne kolory! Spróbuj ponownie.")
+            continue
+
         black, white = CheckGuess(secret_code, guess)
         show_feedback(black, white, tries - (i + 1))
 
         if black == Length:
             show_win()
+            time.sleep(5)
             break
 
     else:
         show_loss(secret_code)
+        time.sleep(5)
