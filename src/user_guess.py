bce3e34074fac09510b2a45fa2003e500234aeff
Patryk Czogała <patrykczogala1810@gmail.com>
2025-06-21 18:49:35 +0200

    Dodanie całości funkcji

--- src/user_guess.py
+++ src/user_guess.py
@@ -5,3 +5,24 @@ def player_guesses_code():
     print("Rozpoczynasz grę! Komputer ustawił tajny kod z kolorów.")
     print("Dostępne kolory: Red, Green, White, Yellow, Orange, Pink, Purple, Blue")
     print("Podawaj kolory oddzielone spacją, np.: Red Green Blue Yellow")
+
+    secret_code = GenerateCode()
+    tries = 10
+
+    for i in range(tries):
+        guess_input = input(f"Próba {i+1}/{tries}: ")
+        guess = guess_input.strip().split()
+
+        if len(guess) != Length:
+            print(f"Musisz podać dokładnie {Length} kolorów.")
+            continue
+
+        black, white = CheckGuess(secret_code, guess)
+        show_feedback(black, white, tries - (i + 1))
+
+        if black == Length:
+            show_win()
+            break
+
+    else:
+        show_loss(secret_code)
